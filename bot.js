d457f927241ea7f992565a49d30857c3b7945ec8
Eric Xu <ericyibei@gmail.com>
2020-07-08 19:49:58 -0400

    New stuff
    deez nuts

--- bot.js
+++ bot.js
@@ -1,1 +1,10 @@
+var twit = require(’twit’);
+var config = require(’./config.js’);
+var Twitter = new twit(config);
 
+var retweet = function() {
+  var params = {
+    q: '#ACNH, #Daisy',
+    result_type: 'recent',
+    lang: 'en'    
+  } 
