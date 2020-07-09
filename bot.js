bfe2cd9409b22d0bd3461c57f1dd51d084dab967
Eric Xu <ericyibei@gmail.com>
2020-07-08 21:09:18 -0400

    Searches for Daisy images only
    look in line 45 for code that does that

--- bot.js
+++ bot.js
@@ -1,10 +1,73 @@
-var twit = require(’twit’);
-var config = require(’./config.js’);
+var twit = require('twit');
+var config = require('./config.js');
 var Twitter = new twit(config);
 
-var retweet = function() {
-  var params = {
-    q: '#ACNH, #Daisy',
+// function hasImage(status)
+// {
+// 	try
+// 	{
+// 		return status.entities.media
+// 	}
+// 	catch(e){};
+// }
+
+var retweet = function() 
+{
+  var params = 
+  {
+    q: '#ACNH, Daisy, -mae',
     result_type: 'recent',
-    lang: 'en'    
-  } 
+    tweet_mode: 'extended',
+    lang: 'en'
+ } 
+
+
+    Twitter.get('search/tweets', params, function(err, data) 
+    {
+    	//console.log(data);
+    	// var imageExist = data.statuses[0].metadata;
+    	 // var extentities = data.statuses[0].entities;
+    	// var indices = entities.user_mentions[0].indices;
+
+         console.log(data.statuses);
+        // return;
+      // if there no errors
+
+
+        if (!err) 
+        {
+          // grab ID of tweet to retweet
+            
+
+            // Tell TWITTER to retweet
+            for(let status of data.statuses)
+            {
+            	if(!status.entities.hasOwnProperty('media'))
+            		continue
+            	let retweetId = status.id_str;
+	            Twitter.post('statuses/retweet/:id', 
+	            {
+
+	                id: retweetId
+	            }, function(err, response) {
+	                if (response) {
+	                    console.log('Retweeted!!!');
+	                }
+	                // if there was an error while tweeting
+	                if (err) 
+	                {
+	                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+	                }
+	                   else 
+			        {
+			          console.log('Something went wrong while SEARCHING...');
+			        }
+	            });
+        }
+        // if unable to Search a tweet
+    }
+            
+    });
+}
+
+retweet();
--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-module.exports = {
+
+module.exports = 
+{
   consumer_key: 'j3gtLvNhQt5DiftpUsFYpkUsP',  
   consumer_secret: 'qGriQqY05JYt5wRR7ZXkr96zYmrfKUlFQ8hs13obGHJBKqun07',
   access_token: '2559164072-Wv0hxMqEzq9y74tzUbXaNC0WF6qi3YSWy9GWpcv',  
