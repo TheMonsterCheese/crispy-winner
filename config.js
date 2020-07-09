cc9e14f603e56eef05ee3cf83d2ce018f32f1eba
Eric Xu <31231648+TheMonsterCheese@users.noreply.github.com>
2020-07-08 21:10:11 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = 
-{
-  consumer_key: 'j3gtLvNhQt5DiftpUsFYpkUsP',  
-  consumer_secret: 'qGriQqY05JYt5wRR7ZXkr96zYmrfKUlFQ8hs13obGHJBKqun07',
-  access_token: '2559164072-Wv0hxMqEzq9y74tzUbXaNC0WF6qi3YSWy9GWpcv',  
-  access_token_secret: 'jHQV34QAva9wP0ay3hNGFIWSEjEEQe1KUZ5YnNgQU8wVu'
-}
